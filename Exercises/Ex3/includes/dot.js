7746c0d4c45bf85846b00c7a4584063ccd89830f
Fadi AT <fadiatamny@gmail.com>
2019-05-12 23:35:00 +0300

    restructured the script

--- Exercises/Ex3/includes/dot.js
+++ Exercises/Ex3/includes/dot.js
@@ -1,25 +1,28 @@
-(function(){
-    var nav = document.getElementsByClassName('btn');
-    console.log(nav);
-    var i = 0;
-    var selected = null;
+var selected = null;
+var nav = document.getElementsByClassName('btn');
+var i = 0;
 
+var onEnter = function(){
+    for(i = 0; i < nav.length; ++i){
+        if(nav[i].className.includes('selected'))
+            nav[i].className = nav[i].className.replace(' selected','');
+    }
+    this.className = this.className + ' selected';  
+};
+
+var onLeave = function(){
+    this.className = this.className.replace(' selected','');
+    selected.className = selected.className + ' selected';
+};
+
+(function(){
     for(i = 0; i < nav.length; ++i){
         if(nav[i].className.includes('selected'))
             selected = nav[i];
     }
 
     for(i = 0; i < nav.length; ++i){
-        nav[i].addEventListener('mouseover', function () {         
-            for(i = 0; i < nav.length; ++i){
-                if(nav[i].className.includes('selected'))
-                    nav[i].className = nav[i].className.replace(' selected','');
-            }
-            this.className = this.className + ' selected';
-        });
-        nav[i].addEventListener('mouseleave', function () {
-            this.className = this.className.replace(' selected','');
-            selected.className = selected.className + ' selected';
-        });
+        nav[i].addEventListener('mouseover', onEnter);
+        nav[i].addEventListener('mouseleave', onLeave);
     }
 })();
