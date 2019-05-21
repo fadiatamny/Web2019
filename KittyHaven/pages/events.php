41efff86810f37bef173dd823c016e0edd5e35af
Fadi AT <fadiatamny@gmail.com>
2019-05-21 03:41:21 +0300

    changed name from upcoming to events

--- KittyHaven/pages/events.php
+++ KittyHaven/pages/events.php
@@ -2,27 +2,27 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 <div id="accordion">
 
-  <h3 class="event-date">Tuesday, October 20, 2015</h3>
+  <h3 class="event-date">Tuesday, October 21, 2015</h3>
   <li class="Oct202015event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">Wednesday, October 21, 2015</h3>
+  <h3 class="event-date">Wednesday, October 20, 2015</h3>
   <li class="Oct212015event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">1</h3>
+  <h3 class="event-date">Sunday, October 18, 2015</h3>
   <li class="1event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">2</h3>
+  <h3 class="event-date">Thursday, October 12, 2015</h3>
   <li class="2event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">3</h3>
+  <h3 class="event-date">Monday, October 10, 2015</h3>
   <li class="3event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
@@ -42,17 +42,17 @@
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">4</h3>
+  <h3 class="event-date">Thursday, October 5, 2015</h3>
   <li class="4event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">5</h3>
+  <h3 class="event-date">Saturday, October 4, 2015</h3>
   <li class="5event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">6</h3>
+  <h3 class="event-date">Sunday, October 3, 2015</h3>
   <li class="6event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
@@ -61,12 +61,12 @@
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">7</h3>
+  <h3 class="event-date">Sunday, October 2, 2015</h3>
   <li class="7event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
 
-  <h3 class="event-date">8</h3>
+  <h3 class="event-date">Sunday, October 1, 2015</h3>
   <li class="8event">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </li>
--- KittyHaven/pages/user.php
+++ KittyHaven/pages/user.php
@@ -1,5 +1,5 @@
 <?php require_once('../templates/head.php');
-printHead('Kitty Profile ;from session name', ['user.css']); ?>
+printHead("KitKatt's Profile", ['user.css']); ?>
 <?php require_once('../templates/navbar.php');
 printNav('https://cdn1.iconfinder.com/data/icons/business-charts/512/customer-512.png') ?>
 <header class="userHeader">
@@ -10,8 +10,8 @@ printNav('https://cdn1.iconfinder.com/data/icons/business-charts/512/customer-51
             </div>
             <div class="col">
                 <div class="text">
-                    <h1>KittyName from session</h1>
-                    <h5>Age: XX | Adopted: XX/XX/XX</h5>
+                    <h1>KitKatt</h1>
+                    <h5>Age: 5 | Adopted: 19/06/1996</h5>
                 </div>
             </div>
         </div>
@@ -26,10 +26,10 @@ printNav('https://cdn1.iconfinder.com/data/icons/business-charts/512/customer-51
     <div class="row">
         <div class="col-12" id="nav">
             <div class="list-group list-inline" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active first" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Overview</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Upcoming</a>
-                <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Update</a>
-                <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Memories</a>
+                <a class="list-group-item list-group-item-action active first" id="list-overview-list" data-toggle="list" href="#list-overview" role="tab" aria-controls="overview">Overview</a>
+                <a class="list-group-item list-group-item-action" id="list-events-list" data-toggle="list" href="#list-events" role="tab" aria-controls="events">Events</a>
+                <a class="list-group-item list-group-item-action" id="list-update-list" data-toggle="list" href="#list-update" role="tab" aria-controls="update">Update</a>
+                <a class="list-group-item list-group-item-action" id="list-memories-list" data-toggle="list" href="#list-memories" role="tab" aria-controls="memories">Memories</a>
             </div>
         </div>
         <div class="col-12" id="navSeperator">
@@ -37,11 +37,12 @@ printNav('https://cdn1.iconfinder.com/data/icons/business-charts/512/customer-51
         </div>
         <div class="col-12" id="data">
             <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list"><?php require_once('../pages/overview.php'); ?></div>
-                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list"><?php require_once('../pages/upcoming.php'); ?></div>
-                <!-- REMOVE THE UPDATE AND MAKE IT A LIGHTBOX IN THE UPCOMING TAB INSTEAD ? -->
-                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list"><?php require_once('../pages/update.php'); ?></div>
-                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list"><?php require_once('../pages/memories.php'); ?></div>
+                <div class="tab-pane fade show active" id="list-overview" role="tabpanel" aria-labelledby="list-overview-list"><?php require_once('../pages/overview.php'); ?></div>
+                <!-- CHANGED NAME TO EVENTS? ? -->
+                <div class="tab-pane fade" id="list-events" role="tabpanel" aria-labelledby="list-events-list"><?php require_once('../pages/events.php'); ?></div>
+                <!-- REMOVE THE UPDATE AND MAKE IT A LIGHTBOX IN THE EVENTS TAB INSTEAD ? -->
+                <div class="tab-pane fade" id="list-update" role="tabpanel" aria-labelledby="list-update-list"><?php require_once('../pages/update.php'); ?></div>
+                <div class="tab-pane fade" id="list-memories" role="tabpanel" aria-labelledby="list-memories-list"><?php require_once('../pages/memories.php'); ?></div>
             </div>
         </div>
     </div>
